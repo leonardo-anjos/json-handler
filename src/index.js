0614830d1e8148d51ee8881db43e288ecc4e8393
Leonardo Anjos <leodrums11@gmail.com>
2024-03-07 10:47:32 -0300

    wip: main function - handler json input data

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const generateRandomData = require('./randomJsonData');
+
+const handlerInputJson = (jsonData) => {
+  if (typeof jsonData !== 'object' || jsonData === null) {
+      throw new Error('input json invalid!');
+  }
+
+  console.log('INPUT: ', JSON.stringify(jsonData, null, 2), );
+
+  for (const key in jsonData) {
+      const value = jsonData[key];
+
+      if (typeof value === 'string' && (value === '' || value === '#')) {
+          delete jsonData[key];
+      } else if (typeof value === 'object' && !Array.isArray(value)) {
+          handlerInputJson(value);
+      } else if (Array.isArray(value)) {
+          jsonData[key] = value.filter(item => item !== '' && item !== '#');
+      }
+  }
+
+  console.log('OUTPUT: ', JSON.stringify(jsonData, null, 2));
+  return jsonData;
+}
+
+handlerInputJson(generateRandomData());
+
+module.exports = handlerInputJson;
